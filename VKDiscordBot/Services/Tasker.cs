5c14210fa363da12def4ebfeb9b6a32f3852b683
Huex <tohuex@gmail.com>
2017-08-09 17:41:24 +0300

    Add remove functions to Tasker

--- VKDiscordBot/Services/Tasker.cs
+++ VKDiscordBot/Services/Tasker.cs
@@ -27,6 +27,24 @@ namespace VKDiscordBot.Services
             task.Start();
         }
 
+        public void Remove(RepetitiveTask task)
+        {
+            if (_tasks.Contains(task))
+            {
+                _tasks.Remove(task);
+                RaiseLog(Discord.LogSeverity.Verbose, $"Removed repetitive task: {task.Id}");
+            }
+        }
+
+        public void Remove(int taskId)
+        {
+            var task = _tasks.Find(t => t.Id == taskId);
+            if (task != null)
+            {
+                Remove(task);
+            }
+        }
+
         public RepetitiveTask Find(Predicate<RepetitiveTask> match)
         {
             return _tasks.Find(match);
